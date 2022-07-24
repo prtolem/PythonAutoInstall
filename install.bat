4608575cf09108ed5978bd3925dbf2ac1970d2ad
Prtolem <66413287+prtolem@users.noreply.github.com>
2022-07-24 11:00:15 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 curl "here link to download python like in example" --output python.exe
 python.exe /passive InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1
 DEL /F /A python.exe
-cl
+cls
